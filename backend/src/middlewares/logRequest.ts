984641935f07aaa8aa2bc51507aab315fdba4b1d
Antônio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-10-28 11:39:42 -0300

    Middleware LogRequest added

---
+++ backend/src/middlewares/logRequest.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+
+function logRequest(request: Request, response: Response, next: NextFunction) {
+
+  const { method, url } = request;
+
+  const logLabel = `[AUDIT] - [${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+
+export { logRequest };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-
+import { logRequest } from '../middlewares/logRequest'
 import { usersRouter } from './users.routes'
 
 const routes = Router();
 
-
-routes.use('/users', usersRouter) 
+routes.use(logRequest);
+routes.use('/users', usersRouter)
 
 export { routes }
